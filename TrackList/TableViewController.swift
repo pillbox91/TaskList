111f3ac010de164777310839c136d0ecedfe86e3
Andrey Averyanov <averyanov1996@gmail.com>
2021-05-06 21:37:48 +0300

    Initial Commit

---
+++ TrackList/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by Андрей Аверьянов on 06.05.2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
